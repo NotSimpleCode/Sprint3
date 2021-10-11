caf561e6989ae1300996f49eb7b2e502aa6fe850
NotSimpleCode <onejobsdiego@gmail.com>
2021-10-11 11:57:46 -0500

    vamos a editar esto
    porque si

--- Ejemplo/app.py
+++ Ejemplo/app.py
@@ -10,12 +10,11 @@ def index():
 def register():
     return render_template("login.html")
 
-
+#hola acabo de editar
 
 @app.route("/login", methods=["GET","POST"])
 def login():
     return render_template("register.html")
-
 
 
 if __name__ == "__main__":
