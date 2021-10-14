ec3b70d11d44c3ef752865fab4cf404363d86782
NotSimpleCode <onejobsdiego@gmail.com>
2021-10-13 22:27:13 -0500

    confirmar en app

--- Ejemplo/app.py
+++ Ejemplo/app.py
@@ -10,13 +10,18 @@ def index():
 def register():
     return render_template("login.html")
 
-#hola acabo de editar
-#vamos a editar otra vez pero desde visual
 @app.route("/login", methods=["GET","POST"])
 def login():
-    return render_template("register.html")
+    return render_template("teacher.html")
+
+@app.route("/login2", methods=["GET","POST"])
+def login2():
+    return render_template("student.html") 
     
-#pull request
+@app.route("/salir", methods=["GET","POST"])
+def salir():
+    return render_template("login.html") 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
