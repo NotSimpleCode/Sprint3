74b99e00ceabc69688d455a8d8cc9f412c2569a8
NotSimpleCode <onejobsdiego@gmail.com>
2021-10-14 13:43:51 -0500

    confirmar update 2

--- Ejemplo/app.py
+++ Ejemplo/app.py
@@ -6,22 +6,21 @@ app=Flask(__name__)
 def index():
     return render_template('register.html')
 
-@app.route("/register", methods=["GET","POST"])
-def register():
-    return render_template("login.html")
-
 @app.route("/login", methods=["GET","POST"])
 def login():
+    return render_template("login.html")
+
+@app.route("/teacher", methods=["GET","POST"])
+def teacher():
     return render_template("teacher.html")
 
-@app.route("/login2", methods=["GET","POST"])
-def login2():
+@app.route("/student", methods=["GET","POST"])
+def student():
     return render_template("student.html") 
-    
-@app.route("/salir", methods=["GET","POST"])
-def salir():
-    return render_template("login.html") 
 
+@app.route("/register", methods=["GET","POST"])
+def register():
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
